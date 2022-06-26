f170837e17b5a25b4391e5c59fe5d01ca794a94d
mostafa-bd <83468869+mostafa-bd@users.noreply.github.com>
2022-06-26 15:20:36 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,144 @@
+
+const CHRHEIGHT = 9;
+const CHRWIDTH = 8;
+const FONT      = '48px monospace';
+const HEIGHT    = 120;
+const WIDTH = 128;
+const MAP_WIDTH = 32;
+const MAP_HEIGHT = 32;
+const SMOOTH = 0;
+const TILECOLUMN = 4;
+const TILEROW = 4;
+const TILESIZE = 8;
+
+let gFrame = 0;
+let gWidth;
+let gHeight;
+let gImgMap;
+let gImgPlayer;
+let gPlayerX = 0;
+let gPlayerY = 0;
+let gScreen;
+
+const gFileMap = 'img/map.png';
+const gFilePlayer = 'img/player.png';
+
+const gMap = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 3, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 3, 6, 3, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 3, 3, 6, 6, 7, 7, 7, 2, 2, 2, 7, 7, 7, 7, 7, 7, 7, 6, 3, 0, 0, 0, 3, 3, 0, 6, 6, 6, 0, 0, 0,
+ 0, 0, 3, 3, 6, 6, 6, 7, 7, 2, 2, 2, 7, 7, 2, 2, 2, 7, 7, 6, 3, 3, 3, 6, 6, 3, 6,13, 6, 0, 0, 0,
+ 0, 3, 3,10,11, 3, 3, 6, 7, 7, 2, 2, 2, 2, 2, 2, 1, 1, 7, 6, 6, 6, 6, 6, 3, 0, 6, 6, 6, 0, 0, 0,
+ 0, 0, 3, 3, 3, 0, 3, 3, 3, 7, 7, 2, 2, 2, 2, 7, 7, 1, 1, 6, 6, 6, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 7, 7, 7, 7, 2, 7, 6, 3, 1, 3, 6, 6, 6, 3, 0, 0, 0, 3, 3, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 7, 2, 7, 6, 3, 1, 3, 3, 6, 6, 3, 0, 0, 0, 3, 3, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 7, 7, 7, 6, 3, 1, 1, 3, 3, 6, 3, 3, 0, 0, 3, 3, 3, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 6, 7, 7, 7, 6, 3, 1, 1, 3, 3, 6, 3, 3, 0, 3,12, 3, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 7, 7, 6, 3, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 6, 6, 6, 6, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 6, 6, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 1, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 5, 3, 3, 3, 6, 6, 6, 3, 3, 3, 1, 1, 1, 1, 1, 3, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 8, 9, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 3, 3, 3, 1, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 3, 3, 3, 3, 3, 3, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 3, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 3, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 3, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 3, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14, 6, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0,
+ 7,15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 0, 0, 0, 0, 0,
+ 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7,];
+
+function DrawMain() {
+  const g = gScreen.getContext('2d');
+    for (let y = 0; y < 20; y++) {
+      for (let x = 0; x < 20; x++) {
+        let px = gPlayerX + x;
+        let py = gPlayerY + y;
+        DrawTile(g, x * TILESIZE - TILESIZE / 2, y * TILESIZE, gMap[py * MAP_WIDTH + px]);
+      }
+  };
+
+  g.fillStyle = '#ff0000';
+  g.fillRect(0, HEIGHT / 2 - 1, WIDTH, 2);
+  g.fillRect(WIDTH / 2 - 1, 0, 2, HEIGHT);
+
+  g.drawImage(gImgPlayer, CHRWIDTH, 0, CHRWIDTH, CHRHEIGHT, WIDTH/2 - CHRWIDTH/2, HEIGHT/2 - CHRHEIGHT + TILESIZE /2, CHRWIDTH, CHRHEIGHT);
+  // g.font = FONT;
+  // g.fillText('Hello World' + gFrame, gFrame/10, 64);
+  };
+  
+function DrawTile(g, x, y, idx) {
+    const ix = (idx % TILECOLUMN) * TILESIZE;
+    const iy = Math.floor(idx / TILECOLUMN) * TILESIZE;
+    g.drawImage(gImgMap, ix, iy, TILESIZE, TILESIZE, x, y, TILESIZE, TILESIZE);
+      };
+
+function LoadImage() {
+   gImgMap = new Image();
+  gImgMap.src = gFileMap;
+   gImgPlayer = new Image();
+  gImgPlayer.src = gFilePlayer;
+}
+
+function WmPaint() {
+  DrawMain();
+  const ca = document.getElementById('main');
+  const g = ca.getContext('2d');
+
+  g.drawImage(gScreen, 0, 0, gScreen.width,gScreen.height, 0, 0, gWidth, gHeight);
+};
+
+function WmSize() {
+    const ca = document.getElementById('main');
+  ca.width = window.innerWidth;
+  ca.height = window.innerHeight;
+
+  const g = ca.getContext('2d');
+  g.imageSmoothingEnabled = g.msimageSmoothingEnabled = SMOOTH;
+
+  gWidth = ca.width;
+  gHeight = ca.height;
+  if (gWidth / WIDTH < gHeight / HEIGHT) {
+    gHeight = gWidth * HEIGHT / WIDTH;
+  } else {
+    gWidth = gHeight * WIDTH / HEIGHT;
+  }
+
+};
+
+function WmTimer() {
+  gFrame++
+
+  WmPaint();
+};
+
+window.onkeydown = function (ev) {
+  
+  let c = ev.keyCode;
+
+  if (c === 37) gPlayerX--;
+  if (c === 38) gPlayerY--;
+  if (c === 39) gPlayerX++;
+  if (c === 40) gPlayerY++;
+}
+
+window.onload = function () {
+
+  LoadImage();
+
+  gScreen = document.createElement('canvas');
+  gScreen.width = WIDTH;
+  gScreen.height = HEIGHT;
+
+  WmSize();
+  window.addEventListener('resize', WmSize)
+  setInterval(function () { WmTimer() }, 33);
+};
